8a2fbb3694879e0b945251d8baa0be4de7330f85
Gustavo Jung <jung.gustavo@gmail.com>
2020-09-29 09:18:15 -0300

    Initial commit

---
+++ Primeiro/Primeiro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+
+                Console.WriteLine("Olá mundo!");
+            }
+        }
+    }
+}
